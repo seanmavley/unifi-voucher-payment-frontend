d596c94a30e707c2ba88c2573ddfdef31bdfa851
Rexford Nkansah <seanmavley@gmail.com>
2017-09-20 21:41:54 +0000

    Cleaned code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,22 +18,7 @@ export class AppComponent {
   error: boolean;
   success: boolean;
 
-  constructor(public purchase: PurchaseService) {
-
-  }
-
-  onTest() {
-    let x = 1;
-    let myInterval = setInterval((doPing) => {
-      console.log('I was logged after 3 seconds');
-      x += 1;
-      if (x > 5) {
-        clearInterval(myInterval);
-        console.log('Canceled interval callbacks');
-      }
-    }, 3000);
-
-  }
+  constructor(public purchase: PurchaseService) { }
 
   onSubmit(data) {
     console.log(data);
@@ -43,20 +28,12 @@ export class AppComponent {
       .subscribe((res) => {
         console.log(res.json());
 
-        // if response is back via 0000
-        if (res.json().state) {
+        if (res.json().state) { // response '0000'
           console.log('Successfully went through', res.json().data.ResponseCode);
           this.busy = false;
           this.success = true;
-        }
-
-        // otherwise
-        if (!res.json().state) {
-          if(res.json().data.ResponseCode === '0001') {
-
-            console.log('pending data, will begin polling now');
-            console.log('polling the endpoint api.khophi.com/hubtel/callback/get');
-
+        } else {
+            if(res.json().data.ResponseCode === '0001') {
             /**
             * Make requests to /callback/get endpoint every 15 seconds
             * If a transaction is found, cancel the interval, otherwise, keep
@@ -66,8 +43,6 @@ export class AppComponent {
               let trans_id = res.json().data.Data.TransactionId;
               this.purchase.callback(trans_id)
                 .subscribe((res) => {
-                  // Callback request found and logged
-                  // Therefore we can make a conclusion
                   if (res.json().status === 400) { // means transaction failed
                     console.log('Forgetti. Failed request');
                     console.log(res.json().error);
@@ -75,16 +50,14 @@ export class AppComponent {
                     this.error = true;
                   }
 
-                  // We got the code generated. We're a go
                   if (res.json().status === 200) {
                     console.log('Good, here is your code');
                     clearInterval(pollInterval);
                     this.success = true;
                   }
 
-                  // the purchase call to the api would still be happening,
-                  // because we assume a 404 each time. We only clear the interval
-                  // when we get a response back
+                  // api polling still happens. We assume 404 each time
+                  // because we assume a 404 each time.
                 })
             }, 15000)
           } else {
@@ -92,7 +65,7 @@ export class AppComponent {
             this.busy = false;
             this.error = true;
           }
-        }
+      }
     })  
   };
 }
